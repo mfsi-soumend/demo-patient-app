fe84499f4baaa53d6f3027c28acbf9ea74d73156
soumendas2252 <70625572+soumendas2252@users.noreply.github.com>
2023-11-24 17:43:48 +0530

    added identifier

--- dboperations.js
+++ dboperations.js
@@ -1,49 +1,70 @@
 const config = require("./dbconfig");
 const mysql = require("mysql");
+const axios = require("axios");
 
 const pool = mysql.createPool(config);
 
 async function addPatientData(Patient) {
   return new Promise((resolve, reject) => {
-    pool.getConnection((err, connection) => {
-      if (err) {
+    let identifier;
+    axios
+      .get(
+        "https://qa-refapp.openmrs.org/openmrs/ws/rest/v1/idgen/nextIdentifier?source=1",
+        {
+          auth: {
+            username: "admin",
+            password: "Admin123",
+          },
+        }
+      )
+      .then((res) => {
+        identifier = res.data.results[0].identifierValue;
+
+        pool.getConnection((err, connection) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+
+          const sqlQuery =
+            "INSERT INTO patient (given_name, middle_name, family_name, birthdate, birthdate_estimated, gender, preferred, address1, city_village, state_province, country, postal_code, phone, is_processed, identifier) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+          connection.query(
+            sqlQuery,
+            [
+              Patient.givenName,
+              Patient.middleName,
+              Patient.familyName,
+              new Date(Patient.birthdate),
+              Patient.birthdateEstimated,
+              Patient.gender,
+              Patient.preferred,
+              Patient.address1,
+              Patient.cityVillage,
+              Patient.stateProvince,
+              Patient.country,
+              Patient.postalCode,
+              Patient.phone,
+              false,
+              identifier,
+            ],
+            (error, results, fields) => {
+              connection.release();
+
+              if (error) {
+                reject(error);
+              } else {
+                console.log(results);
+                resolve(results);
+              }
+            }
+          );
+        });
+      })
+      .catch((err) => {
         reject(err);
         return;
-      }
-
-      const sqlQuery =
-        "INSERT INTO patient (given_name, middle_name, family_name, birthdate, birthdate_estimated, gender, preferred, address1, city_village, state_province, country, postal_code, phone, is_processed) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-
-      connection.query(
-        sqlQuery,
-        [
-          Patient.givenName,
-          Patient.middleName,
-          Patient.familyName,
-          new Date(Patient.birthdate),
-          Patient.birthdateEstimated,
-          Patient.gender,
-          Patient.preferred,
-          Patient.address1,
-          Patient.cityVillage,
-          Patient.stateProvince,
-          Patient.country,
-          Patient.postalCode,
-          Patient.phone,
-          false,
-        ],
-        (error, results, fields) => {
-          connection.release();
-
-          if (error) {
-            reject(error);
-          } else {
-            console.log(results);
-            resolve(results);
-          }
-        }
-      );
-    });
+      });
   });
 }
 
